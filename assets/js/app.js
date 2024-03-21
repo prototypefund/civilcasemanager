045748f86fb5fdb1d57d251a1900e81fe20cbf2c
dev-sw-1 <21968119-dev-sw-1@users.noreply.gitlab.com>
2024-03-21 10:54:58 +0100

    Add fade in

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,10 +22,20 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+let Hooks = {};
+
+Hooks.FadeIn = {
+  updated() {
+    console.log("UPDATE")
+    this.el.classList.add("fade-in");
+  }
+};
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken}
+  params: {_csrf_token: csrfToken},
+  hooks: Hooks
 })
 
 // Show progress bar on live navigation and form submits
@@ -41,4 +51,5 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+
 

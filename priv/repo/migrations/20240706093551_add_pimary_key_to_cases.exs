c0f983a42232228b35779ca803a5553e32302e23
dev-sw-1 <21968119-dev-sw-1@users.noreply.gitlab.com>
2024-07-06 11:48:58 +0200

    Add primary key to cases

---
+++ priv/repo/migrations/20240706093551_add_pimary_key_to_cases.exs
@@ -0,0 +1,9 @@
+defmodule Events.Repo.Migrations.AddPimaryKeyToCases do
+  use Ecto.Migration
+
+  def change do
+    alter table(:cases) do
+      modify :id, :string, primary_key: true
+    end
+  end
+end

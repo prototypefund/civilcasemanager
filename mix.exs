db15d48e3e99f960f62beea47ccf557347244fff
dev-sw-1 <21968119-dev-sw-1@users.noreply.gitlab.com>
2024-07-07 21:20:42 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule CaseManager.MixProject do
        depth: 1},
       {:swoosh, "~> 1.5"},
       {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},

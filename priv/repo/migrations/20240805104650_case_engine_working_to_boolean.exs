030d5808ccf575e13102b89ff1c844389f8745c5
dev-sw-1 <21968119-dev-sw-1@users.noreply.gitlab.com>
2024-08-08 23:33:15 +0200

    Don't use custom type

--- priv/repo/migrations/20240805104650_case_engine_working_to_boolean.exs
+++ priv/repo/migrations/20240805104650_case_engine_working_to_boolean.exs
@@ -2,11 +2,9 @@ defmodule CaseManager.Repo.Migrations.CaseEngineFailureToBoolean do
   use Ecto.Migration
 
   def change do
-    execute "CREATE TYPE tristate AS ENUM ('yes', 'no', 'unknown')"
-
     alter table(:cases) do
       remove :boat_engine_working
-      add :boat_engine_working, :tristate
+      add :boat_engine_working, :string
     end
 
     alter table(:imported_cases) do

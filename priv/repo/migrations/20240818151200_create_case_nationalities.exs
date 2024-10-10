3cd77821cba2202a8d13a7cddce5cb536424a170
dev-sw-1 <21968119-dev-sw-1@users.noreply.gitlab.com>
2024-10-10 23:45:11 +0200

    Set primary key for case_nationalities

--- priv/repo/migrations/20240818151200_create_case_nationalities.exs
+++ priv/repo/migrations/20240818151200_create_case_nationalities.exs
@@ -3,9 +3,12 @@ defmodule CaseManager.Repo.Migrations.CreateCaseNationalities do
 
   def change do
     create table(:case_nationalities, primary_key: false) do
-      add :country, :string, size: 2, null: false
+      add :country, :string, size: 2, null: false, primary_key: true
       add :count, :integer
-      add :case_id, references(:cases, on_delete: :nothing, type: :string), null: false
+
+      add :case_id, references(:cases, on_delete: :nothing, type: :string),
+        null: false,
+        primary_key: true
     end
 
     create unique_index(:case_nationalities, [:country, :case_id])
